7500d36359a8fa106ea1f0209ca3ace50cf1a279
EvanChen <cczapp@163.com>
2022-01-14 09:07:42 +0800

    feat: init

---
+++ Person.ts
@@ -0,0 +1,5 @@
+export interface Person {
+    code?:    number;
+    message?: string;
+    type?:    string;
+}
---
+++ src/requestData.ts
@@ -0,0 +1,8 @@
+import axios from 'axios'
+import fs from 'fs'
+
+export default async (url: string) => {
+  const res = await axios.get(url)
+  fs.writeFileSync('./swagger.json', JSON.stringify(res.data, null, '\t'))
+  return res.data
+}
