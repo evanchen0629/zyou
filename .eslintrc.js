ea8d42568a062fb6e6a5517829bd9180881a5eca
EvanChen <cczapp@163.com>
2022-01-22 11:56:58 +0800

    feat: add init config&add eslintignore

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,3 @@
 module.exports = {
-  extends: ["plugin:prettier/recommended", "prettier"],
-};
+  extends: ['plugin:prettier/recommended', 'prettier'],
+}
--- src/init/index.ts
+++ src/init/index.ts
@@ -1,3 +1,15 @@
+import path from 'path'
+import fs from 'fs'
+import colors from 'colors'
+
 export default () => {
-  console.log('功能未实现: )')
+  const typesConfig = fs.readFileSync(
+    path.join(path.dirname(__dirname), '../typesConfig.json'),
+    { encoding: 'utf8' }
+  )
+  fs.writeFileSync(
+    path.join(path.resolve(process.cwd()), 'typesConfig.json'),
+    typesConfig
+  )
+  console.log(colors.green('初始化配置文件成功'))
 }
