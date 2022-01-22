8737f86aaa37129d3c654d8f9aeb5ef0fe2d9f42
EvanChen <cczapp@163.com>
2022-01-22 11:34:30 +0800

    feat: update version

--- src/generateAll/index.ts
+++ src/generateAll/index.ts
@@ -3,6 +3,8 @@ import generateDefinitions from './generateDefinitions'
 import getConfig from './getConfig'
 import requestData from './requestData'
 import colors from 'colors'
+import fs from 'fs'
+import { typeMapping } from './writeData'
 
 async function generateAll() {
   const config = getConfig()
@@ -10,6 +12,7 @@ async function generateAll() {
   const data = await requestData(config.path)
   generateDefinitions(data)
   process.on('exit', function () {
+    // TODO: 程序结束时(读写文件完成), 要去重
     console.log(
       `The source code is at ${colors.green(
         'https://github.com/evanchen0629/zyou'
